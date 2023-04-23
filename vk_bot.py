0b3dbfa516676f874a6b130b8c090cf8eeaf9d64
Oleg <olberd@bk.ru>
2023-04-23 15:55:14 +0300

    Add DialogFlow to VK bot.

--- vk_bot.py
+++ vk_bot.py
@@ -1,21 +1,26 @@
 import os
-
-import vk_api
+import random
+import vk_api as vk
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
+from intent_utils import detect_intent_texts
 
-load_dotenv()
-vk_token = os.getenv('VK_TOKEN')
 
-vk_session = vk_api.VkApi(token=vk_token)
+def main():
+    load_dotenv()
+    vk_token = os.getenv('VK_TOKEN')
+    vk_session = vk.VkApi(token=vk_token)
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            answer = detect_intent_texts('game-of-vebs-bssh', event.user_id, [event.text], 'ru-RU')
+            vk_api.messages.send(
+                user_id=event.user_id,
+                message=answer,
+                random_id=random.randint(1, 1000)
+            )
 
-longpoll = VkLongPoll(vk_session)
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+if __name__ == "__main__":
+    main()
